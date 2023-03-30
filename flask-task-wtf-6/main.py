9792a0b84c988da61b6d4ea527f5420479cf3b40
Nikita <117333817+SanctusBebrus@users.noreply.github.com>
2023-03-30 15:06:51 +1000

    Update main.py

--- flask-task-wtf-6/main.py
+++ flask-task-wtf-6/main.py
@@ -1,12 +1,14 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/training/<prof>')
-def index(prof):
-    return render_template('base.html', prof=prof)
+@app.route('/list_prof/<order>')
+def news(type_printing):
+    profession_list = ['Конченый идиот', 'Самый сексуальный мужик в мире', 'Горячая чикса',
+                       'Злодей британец', 'Так себе шутник', 'Пубертатная язва',
+                       'Какой-то мужик', 'Говнюки', 'Недопонятый гений']
+    return render_template('base.html', prof_list=profession_list, print_type=order)
 
 
 if __name__ == '__main__':
